d707fb685f27e9dc8823f8f4ab58374c9d7d6065
maggiebarker <maggieb.06@gmail.com>
2017-10-22 22:51:21 -0600

    style updates

--- assets/javascript/wed.js
+++ assets/javascript/wed.js
@@ -1,43 +1,37 @@
 $(document).ready(function() {
-// Set the date we're counting down to
+
+//Countdown
 var countDownDate = new Date("March 30, 2018 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
-
-  // Get todays date and time
+// Get todays date and time
   var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
+// Find the distance between now an the count down date
   var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
+// Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
+// Display the result in the element with id="demo"
   document.getElementById("clock").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text 
+// If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("clock").innerHTML = "EXPIRED";
+    document.getElementById("clock").innerHTML = "Just Married!";
   }
 }, 1000);
 
 
 //Slideshow
 var images = ["assets/images/columbiagorge.jpg", "assets/images/dammm.jpg", "assets/images/eastcanyon.jpg", "assets/images/gala.jpg", "assets/images/lanterns.jpg", "assets/images/tricksy.jpg", "assets/images/beachboys.jpg", "assets/images/vegas.jpg", "assets/images/zoobrew.jpg", "assets/images/saltlake.jpg"];
-
 var showImage;
-
 var count = 0;
 
 $("#start").click(startSlideshow);
-
 $("#stop").click(stopSlideshow);
 
 function displayImage() {
@@ -46,22 +40,17 @@ function displayImage() {
 
 function nextImage() {
   count++;
-
   $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
-
   setTimeout(displayImage, 1000);
   if (count === images.length) {
     count = 0;
   }
 }
-
 function startSlideshow() {
   showImage = setInterval(nextImage, 3000);
 }
-
 function stopSlideshow() {
   clearInterval(showImage);
 }
-
 displayImage();
 });
