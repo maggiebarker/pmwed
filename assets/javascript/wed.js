92451afacc616bea79de095cd54c870fdf7e7508
maggiebarker <maggieb.06@gmail.com>
2017-11-25 23:29:36 -0700

    new jumbotron image, new photos

--- assets/javascript/wed.js
+++ assets/javascript/wed.js
@@ -2,32 +2,31 @@ $(document).ready(function() {
 
 //Countdown
 var countDownDate = new Date("March 30, 2018 19:00:00").getTime();
-
 // Update the count down every 1 second
 var x = setInterval(function() {
 // Get todays date and time
-  var now = new Date().getTime();
+var now = new Date().getTime();
 // Find the distance between now an the count down date
-  var distance = countDownDate - now;
+var distance = countDownDate - now;
 // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 // Display the result in the element with id="demo"
-  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  $("#clock").text(days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s");
 // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("clock").innerHTML = "Just Married!";
+    $("#clock").text("Just Married!");
   }
 }, 1000);
 
 
 //Slideshow
-var images = ["assets/images/columbiagorge.jpg", "assets/images/dammm.jpg", "assets/images/eastcanyon.jpg", "assets/images/gala.jpg", "assets/images/lanterns.jpg", "assets/images/tricksy.jpg", "assets/images/beachboys.jpg", "assets/images/vegas.jpg", "assets/images/zoobrew.jpg", "assets/images/saltlake.jpg"];
+var images = ["assets/images/columbiagorge.jpg", "assets/images/dammm.jpg", "assets/images/eastcanyon.jpg", "assets/images/gala.jpg", "assets/images/lanterns.jpg", "assets/images/morevegas.jpg", "assets/images/beachboys.jpg", "assets/images/vegas.jpg", "assets/images/zoobrew.jpg", "assets/images/saltlake.jpg"];
 var showImage;
 var count = 0;
 
@@ -40,14 +39,13 @@ function displayImage() {
 
 function nextImage() {
   count++;
-  $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
   setTimeout(displayImage, 1000);
   if (count === images.length) {
     count = 0;
   }
 }
 function startSlideshow() {
-  showImage = setInterval(nextImage, 3000);
+  showImage = setInterval(nextImage, 2000);
 }
 function stopSlideshow() {
   clearInterval(showImage);
